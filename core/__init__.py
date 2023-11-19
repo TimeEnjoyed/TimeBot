f558b7006ea5c4f1c3df76b71eb63d5a07736ae2
EvieePy <evieepy@gmail.com>
2023-11-19 23:15:49 +1000

    Initial Database base.

--- core/__init__.py
+++ core/__init__.py
@@ -12,5 +12,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+from .api import *
 from .bots import *
 from .config import config as config
+from .database import Database as Database
