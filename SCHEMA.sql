ff364170bcb28973bee91d61f822b11574b04d30
EvieePy <evieepy@gmail.com>
2023-11-20 09:59:42 +1000

    Update schema

--- SCHEMA.sql
+++ SCHEMA.sql
@@ -1,8 +1,14 @@
 CREATE TABLE IF NOT EXISTS users (
     uid BIGINT PRIMARY KEY,
-    discord_id BIGINT,
-    twitch_id BIGINT,
+    discord_id BIGINT UNIQUE,
+    twitch_id BIGINT UNIQUE,
     moderator BOOLEAN NOT NULL DEFAULT false,
-    token TEXT NOT NULL,
+    token TEXT UNIQUE NOT NULL,
     created TIMESTAMP DEFAULT (now() at time zone 'utc')
+);
+
+CREATE TABLE IF NOT EXISTS state (
+    code TEXT PRIMARY KEY,
+    discord_id BIGINT UNIQUE NOT NULL,
+    moderator BOOLEAN DEFAULT false
 );
