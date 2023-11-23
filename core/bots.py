455af218ed780b1d1d9f3e9cb343e3bb4732744d
EvieePy <evieepy@gmail.com>
2023-11-23 17:17:03 +1000

    Add wavelink connection

--- core/bots.py
+++ core/bots.py
@@ -19,6 +19,7 @@ import pathlib
 from typing import TYPE_CHECKING
 
 import discord
+import wavelink
 from discord.ext import commands
 from twitchio.ext import commands as tcommands
 
@@ -49,6 +50,9 @@ class DiscordBot(commands.Bot):
         logger.info(f"Logged into Discord as {self.user} | {self.user.id}")
 
     async def setup_hook(self) -> None:
+        node: wavelink.Node = wavelink.Node(uri=config["WAVELINK"]["uri"], password=config["WAVELINK"]["password"])
+        await wavelink.Pool.connect(nodes=[node], client=self, cache_capacity=100)
+        
         location = ("extensions/discord", "extensions.discord")
         extensions: list[str] = [f"{location[1]}.{f.stem}" for f in pathlib.Path(location[0]).glob("*.py")]
 
@@ -56,6 +60,10 @@ class DiscordBot(commands.Bot):
             await self.load_extension(extension)
 
         logger.info("Loaded extensions for Discord Bot.")
+    
+    async def on_wavelink_node_ready(self, payload: wavelink.NodeReadyEventPayload) -> None:
+        node: wavelink.Node = payload.node
+        logger.info("Wavelink successfully connected: %s. Resumed: %s", node.identifier, payload.resumed)
 
 
 class TwitchBot(tcommands.Bot):
