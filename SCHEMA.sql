0c6091c104de853fa8106a3f773ba5893d5e00f4
timeenjoyed <timeenjoyed.twitch@gmail.com>
2024-02-26 23:49:36 -0800

    user database add points col

--- SCHEMA.sql
+++ SCHEMA.sql
@@ -1,11 +1,12 @@
--- Active: 1699682544906@@localhost@5432@timeenjoyed
+-- Active: 1703653682760@@127.0.0.1@5432@timebot_test
 CREATE TABLE IF NOT EXISTS users (
     uid BIGINT PRIMARY KEY,
     discord_id BIGINT UNIQUE,
     twitch_id BIGINT UNIQUE,
     moderator BOOLEAN NOT NULL DEFAULT false,
     token TEXT UNIQUE NOT NULL,
-    created TIMESTAMP DEFAULT (now() at time zone 'utc')
+    created TIMESTAMP DEFAULT (now() at time zone 'utc'),
+    points BIGINT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS state (
